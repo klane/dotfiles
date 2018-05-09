c745526e22da2172586a223ff980503606f0a99b
Kevin Lane <lane.kevin.a@gmail.com>
2018-05-09 14:06:01 -0700

    Comment out most lines to test Cygwin setup

--- install.ps1
+++ install.ps1
@@ -1,35 +1,35 @@
 ﻿# add necessary buckets
-scoop bucket add extras
-scoop bucket add versions
+#scoop bucket add extras
+#scoop bucket add versions
 
 # install common programs
-scoop install ln
-scoop install sudo
-scoop install which
-sudo scoop install 7zip --global
-sudo scoop install git --global
+#scoop install ln
+#scoop install sudo
+#scoop install which
+#sudo scoop install 7zip --global
+#sudo scoop install git --global
 
 # install languages
-scoop install miniconda3
-scoop install python27
+#scoop install miniconda3
+#scoop install python27
 scoop install python
-scoop install r
+#scoop install r
 
 # install developer tools
-scoop install atom
-scoop install cmder
-scoop install gitkraken
-scoop install jetbrains-toolbox
-scoop install notepadplusplus
-scoop install sourcetree
-scoop install sqlitebrowser
-scoop install sublime-text
-scoop install vscode
+#scoop install atom
+#scoop install cmder
+#scoop install gitkraken
+#scoop install jetbrains-toolbox
+#scoop install notepadplusplus
+#scoop install sourcetree
+#scoop install sqlitebrowser
+#scoop install sublime-text
+#scoop install vscode
 
 # install other programs
 scoop install cygwin
-scoop install flux
-scoop install msys2
+#scoop install flux
+#scoop install msys2
 
 # install additional cygwin programs
 & "$env:SCOOP\apps\cygwin\setup-x86_64.exe" -n -q -P curl,fish,fontconfig,wget,zsh
