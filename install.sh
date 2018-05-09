5b471c686492b0125158fad5623ba5fd43920c52
Kevin Lane <lane.kevin.a@gmail.com>
2018-05-09 12:13:25 -0700

    Avoid changing directories

--- install.sh
+++ install.sh
@@ -7,18 +7,14 @@ FISHCMPL=$FISHDIR/completions
 [ ! -d $FISHCMPL ] && mkdir $FISHCMPL
 
 git clone https://github.com/klane/dotfiles.git $DIR
-cd $DIR
 
-ln -s .zshrc ~
-ln -s config.fish $FISHDIR
+ln -s $DIR/.zshrc ~
+ln -s $DIR/config.fish $FISHDIR
 echo "eval (pipenv --completion)" > $FISHCMPL/pipenv.fish
 
 if [ "$(uname)" == "Darwin" ]; then
     # install homebrew
 	# link Mac-specific files
 else # assume Windows
-    ln -s .minttyrc ~
+    ln -s $DIR/.minttyrc ~
 fi
-
-# return to home directory
-cd ~
