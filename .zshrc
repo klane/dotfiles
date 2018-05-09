be209b1a107cbde14638b4521ce129f652a12773
Kevin Lane <lane.kevin.a@gmail.com>
2018-05-09 07:34:51 -0700

    Add shell files and Windows installation script

---
+++ .zshrc
@@ -0,0 +1,30 @@
+# The following lines were added by compinstall
+
+zstyle ':completion:*' completer _complete _ignored _approximate
+zstyle ':completion:*' max-errors 2
+zstyle :compinstall filename '/home/Kev/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory autocd
+bindkey -e
+# End of lines configured by zsh-newuser-install
+
+#export PATH=/cygdrive/c/Users/$USER/scoop/shims:$PATH
+export PATH=/cygdrive/c/Users/$USER/scoop/apps/python/current:$PATH
+export PATH=/cygdrive/c/Users/$USER/scoop/apps/python/current/Scripts:$PATH
+source ~/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Load the theme.
+antigen theme agnoster
+
+# Tell Antigen that you're done.
+antigen apply
---
+++ install.ps1
@@ -0,0 +1,46 @@
+﻿# add necessary buckets
+scoop bucket add extras
+scoop bucket add versions
+
+# install common programs
+scoop install ln
+scoop install sudo
+scoop install which
+sudo scoop install 7zip --global
+sudo scoop install git --global
+
+# install languages
+scoop install miniconda3
+scoop install python27
+scoop install python
+scoop install r
+
+# install developer tools
+scoop install atom
+scoop install cmder
+scoop install gitkraken
+scoop install jetbrains-toolbox
+scoop install notepadplusplus
+scoop install sourcetree
+scoop install sqlitebrowser
+scoop install sublime-text
+scoop install vscode
+
+# install other programs
+scoop install flux
+scoop install msys2
+
+#
+sudo choco install cygwin -y
+sudo choco install cyg-get -y
+cyg-get curl
+cyg-get fontconfig
+cyg-get wget
+cyg-get fish
+cyg-get zsh
+
+pip install flake8
+pip install pylint
+pip install autopep8
+pip install pipenv
+pip install powerline-status
