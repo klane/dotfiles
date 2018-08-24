921ab4f549ff7e4e3ce2c6d52287ef1085c48464
Kevin Lane <lane.kevin.a@gmail.com>
2018-08-24 10:52:29 -0700

    Add Scoop global apps folder to PATH

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 switch (string lower $OS)
 case "*windows*"
+    set PATH /cygdrive/c/ProgramData/scoop/shims $PATH
     set PATH /cygdrive/c/Users/$USER/scoop/shims $PATH
     set PATH /cygdrive/c/Users/$USER/scoop/apps/python/current $PATH
     set PATH /cygdrive/c/Users/$USER/scoop/apps/python/current/Scripts $PATH
