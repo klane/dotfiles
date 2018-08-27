82c2196e2f1c9b4ea12b4d820bfb7201e9f9bcdf
Kevin Lane <lane.kevin.a@gmail.com>
2018-08-27 08:54:27 -0700

    Cleanup installation shell script and improve performance on Windows
    - Clone repo with Windows git
    - Remove prepending Scoop to path
    - Force Python 3 for pip upgrade

--- install.sh
+++ install.sh
@@ -5,39 +5,46 @@ gecho () {
     echo "${GREEN}$1${RESET}"
 }
 
- cd ~ # ensure installation starts in the home directory
+cd ~ # ensure installation starts in the home directory
 
 DIR=./project/dotfiles
 FISHDIR=./.config/fish
 FISHCMPL=$FISHDIR/completions
+OSLOW=$(echo $OS | awk '{print tolower($0)}')
+REPO=https://github.com/klane/dotfiles.git
+GITCONFIG=https://raw.githubusercontent.com/klane/dotfiles/master/.gitconfig
 
 gecho 'Creating directories'
 [ ! -d project ] && mkdir project
 [ ! -d $FISHCMPL ] && mkdir -p $FISHCMPL
 
+gecho 'Copying .gitconfig'
+wget --no-hsts $GITCONFIG
+
 gecho 'Cloning repository'
 cd project
-git clone https://github.com/klane/dotfiles.git
+if [[ $OSLOW == *windows* ]]; then
+    /cygdrive/c/ProgramData/scoop/shims/git clone $REPO
+else
+    git clone $REPO
+fi
 cd ~
+rm .gitconfig
 
 gecho 'Linking files'
+#TODO use rsync instead
 ln $DIR/.gitconfig ~
 ln $DIR/.zshrc ~
 ln $DIR/config.fish $FISHDIR
-echo "eval (pipenv --completion)" > $FISHCMPL/pipenv.fish
+echo 'eval (pipenv --completion)' > $FISHCMPL/pipenv.fish
 
-# if [ "$(uname)" == "Darwin" ]; then
-    # install homebrew
-    # link Mac-specific files
-# else
-    # assume Windows
-ln $DIR/.minttyrc ~
-export PATH=/cygdrive/c/Users/$USER/scoop/shims:$PATH
-# fi
+if [[ $OSLOW == *windows* ]]; then
+    ln $DIR/.minttyrc ~
+fi
 
 # install Python packages
 gecho 'Upgrading pip and installing Python packages'
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r $DIR/requirements.txt
 
 # install Powerline fonts
