4d3454f29d52e2ceca3c5d1b00860dd1f35c3bd3
Kevin Lane <lane.kevin.a@gmail.com>
2018-10-08 09:42:36 -0700

    Delete desired commands from fish history and add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,14 @@ case '*'
 
 end
 
+function ignorehistory --on-event fish_prompt
+    echo all | history --delete df du fg bg ls la ll which exit > /dev/null
+end
+
+alias df 'df -h'
+alias du 'du -h'
 alias git hub
+
 set -x PIPENV_VENV_IN_PROJECT true
 set -x SHELL fish
 set fish_greeting
