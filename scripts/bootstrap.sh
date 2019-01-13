74ffb414f7c372811deeb34fff76767e7a8b52ea
Kevin Lane <lane.kevin.a@gmail.com>
2019-01-13 15:06:34 -0800

    Update bootstrap file command spacing

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source support.sh # load support functions & variables
-
 rsync -a --exclude-from="$DIR/support/rsync-exclude.txt" --link-dest=$DIR $DIR/ ~
+
 if is_windows; then
 	ln $DIR/.minttyrc ~
 else
