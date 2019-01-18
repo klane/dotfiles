c8a7612cecc7178ea69f86f3d89e8f695fce824b
Kevin Lane <lane.kevin.a@gmail.com>
2019-01-18 08:49:20 -0800

    Print progress messages in update shell script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+# get repo location
+DIR="${BASH_SOURCE%/*/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
-# update Homebrew
+. $DIR/scripts/support.sh # load support functions & variables
+
+gecho 'Update Homebrew'
 brew update
 
-# upgrade outdated brews and casks
+gecho 'Upgrade outdated brews and casks'
 brew upgrade
 brew cask upgrade
 
-# cleanup brew cached downloads
+gecho 'Cleanup brew cached downloads'
 brew cleanup
 brew prune
