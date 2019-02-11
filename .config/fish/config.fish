1d7c284c945bd7c6ae3f5a60db85cd89d80f867c
Kevin Lane <lane.kevin.a@gmail.com>
2019-02-10 19:20:17 -0800

    Set fish path in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,17 @@ function ignorehistory --on-event fish_prompt
     echo all | history --delete -p $tmp > /dev/null
 end
 
+switch (uname)
+    case Darwin
+        set PATH /usr/local/sbin $PATH
+        set PATH /usr/local/opt/python/libexec/bin $PATH
+        set PATH $HOME/.poetry/bin $PATH
+    case '*'
+        set PATH $PATH /c/Users/$USER/scoop/shims
+        set PATH $PATH /c/Users/$USER/scoop/apps/oraclejdk8/current/bin
+        set PATH $PATH /c/Users/$USER/scoop/apps/python/current/Scripts
+end
+
 alias df 'df -h'
 alias du 'du -h'
 alias git hub
