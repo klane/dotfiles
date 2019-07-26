02b7e65c2663c473e905bf74b40517da67f23739
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-26 12:28:13 -0700

    Move fish path configuration to addpath

--- shells/fish/addpath.fish
+++ shells/fish/addpath.fish
@@ -1,8 +1,11 @@
-switch (string lower $OS)
-case '*windows*'
-    set PATH $PATH /c/Users/$USER/scoop/shims
-    set PATH $PATH /c/Users/$USER/scoop/apps/oraclejdk8/current/bin
-    set PATH $PATH /c/Users/$USER/scoop/apps/python/current/Scripts
-case '*'
-
+switch (uname)
+    case Darwin
+        set PATH /usr/local/sbin $PATH
+        set PATH /usr/local/opt/ruby/bin $PATH
+        set PATH (gem environment gemdir)/bin $PATH
+        set PATH $HOME/.poetry/bin $PATH
+    case '*'
+        set PATH $PATH /c/Users/$USER/scoop/shims
+        set PATH $PATH /c/Users/$USER/scoop/apps/oraclejdk8/current/bin
+        set PATH $PATH /c/Users/$USER/scoop/apps/python/current/Scripts
 end
--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -3,18 +3,6 @@ function ignorehistory --on-event fish_prompt
     echo all | history --delete -p $tmp > /dev/null
 end
 
-switch (uname)
-    case Darwin
-        set PATH /usr/local/sbin $PATH
-        set PATH /usr/local/opt/ruby/bin $PATH
-        set PATH (gem environment gemdir)/bin $PATH
-        set PATH $HOME/.poetry/bin $PATH
-    case '*'
-        set PATH $PATH /c/Users/$USER/scoop/shims
-        set PATH $PATH /c/Users/$USER/scoop/apps/oraclejdk8/current/bin
-        set PATH $PATH /c/Users/$USER/scoop/apps/python/current/Scripts
-end
-
 alias df 'df -h'
 alias du 'du -h'
 alias git hub
