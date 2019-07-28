fa1b8df1748234491d4fae88e9c931cc847fc504
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-27 18:31:03 -0700

    Uninstall Homebrew Ruby

--- shells/fish/addpath.fish
+++ shells/fish/addpath.fish
@@ -1,8 +1,6 @@
 switch (uname)
     case Darwin
         set PATH /usr/local/sbin $PATH
-        set PATH /usr/local/opt/ruby/bin $PATH
-        set PATH (gem environment gemdir)/bin $PATH
         set PATH $HOME/.poetry/bin $PATH
     case '*'
         set PATH $PATH /c/Users/$USER/scoop/shims
