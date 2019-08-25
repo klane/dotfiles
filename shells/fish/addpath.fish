2ce57d3842ecddd24b4982feeed357ac46ccd568
Kevin Lane <lane.kevin.a@gmail.com>
2019-08-24 21:58:03 -0700

    Add paths to dupes that are not automatically linked

--- shells/fish/addpath.fish
+++ shells/fish/addpath.fish
@@ -2,6 +2,12 @@ switch (uname)
     case Darwin
         set PATH /usr/local/sbin $PATH
         set PATH $HOME/.poetry/bin $PATH
+        set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+        set PATH /usr/local/opt/curl/bin $PATH
+        set PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+        set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+        set PATH /usr/local/opt/gnu-which/libexec/gnubin $PATH
+        set PATH /usr/local/opt/grep/libexec/gnubin $PATH
     case '*'
         set PATH $PATH /c/Users/$USER/scoop/shims
         set PATH $PATH /c/Users/$USER/scoop/apps/oraclejdk8/current/bin
