e2e341c800af7498d29ed595754e97d138c13466
Kevin Lane <lane.kevin.a@gmail.com>
2019-11-24 10:47:54 -0800

    Change Hyper cursor color

--- apps/hyper/hyper.js
+++ apps/hyper/hyper.js
@@ -27,7 +27,7 @@ module.exports = {
     letterSpacing: 0,
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: '#61BA86',
 
     // terminal text color under BLOCK cursor
     cursorAccentColor: '#000',
